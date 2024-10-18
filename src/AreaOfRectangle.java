dee24b84c155b0f76379c62caf99b79e25c2a81a
mahimaramesh <mahimapranavi.work@gmail.com>
2024-10-18 12:06:54 +0530

    first commit

---
+++ src/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double length = sc.nextDouble();
+        double width = sc.nextDouble();
+        System.out.printf("%.2f", length * width);
+    }
+}
---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+        double area = ((double) 1 /2) * base * height;
+        System.out.println(area);
+    }
+}
