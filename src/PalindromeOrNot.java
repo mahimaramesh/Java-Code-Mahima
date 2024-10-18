a5f585cf122cb81f64938c2f5d827cb116aa0ad8
mahimaramesh <mahimapranavi.work@gmail.com>
2024-10-18 11:49:27 +0530

    first commit

--- src/PalindromeOrNot.java
+++ src/PalindromeOrNot.java
@@ -4,22 +4,27 @@ public class PalindromeOrNot {
     public static boolean isPalindrome(String name){
         int start = 0;
         int end = name.length() - 1;
+        while(start < end) {
+            if(name.charAt(start) != name.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         name = name.toLowerCase();
-        String copy = name;
-        String rev = "";
 
-
-        while(start < end){
-            rev = rev + end;
-            start++;
-            end--;
+        if(isPalindrome(name)){
+            System.out.println("Palindrome");
         }
-
+        else{
+            System.out.println("Not Palindrome");
+        }
 
     }
 }
