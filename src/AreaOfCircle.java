3689d458fb4b7f7b111c4a2edd4399b8d8805828
mahimaramesh <mahimapranavi.work@gmail.com>
2024-10-18 12:35:22 +0530

    first commit

--- src/AreaOfCircle.java
+++ src/AreaOfCircle.java
@@ -4,7 +4,7 @@ public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int radius = sc.nextInt();
-        double area = 2 * Math.PI * radius;
+        double area = Math.PI * (radius * radius);
         System.out.printf("%.2f" , area);
     }
 }
