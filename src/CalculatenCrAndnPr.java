b2fcadf4d676d399bcb871902fbc2fe772dc044a
mahimaramesh <mahimapranavi.work@gmail.com>
2024-10-22 14:33:40 +0530

    first commit

---
+++ src/CalculatenCrAndnPr.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CalculatenCrAndnPr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        int nCr = fact(n) / (fact(r) * fact(n-r));
+        System.out.println(nCr);
+
+        int nPr = fact(n) / fact(n-r);
+        System.out.println(nPr);
+    }
+    static int fact(int num){
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
